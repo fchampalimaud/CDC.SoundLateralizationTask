51c8cef81aa0d4b98cc93c8f590606f103081972
José Grilo <jose.grilo@research.fchampalimaud.org>
2025-03-05 17:00:13 +0000

    Added quick prompt in the startup script

--- python/src/parser/startup_script.py
+++ python/src/parser/startup_script.py
@@ -12,51 +12,62 @@ def startup():
     with open(config["animal_path"], "r") as file:
         animal_config = yaml.safe_load(file)
 
-    animal_dir = config["output_path"] + "/Rat" + f"{animal_config["animal_id"]:03}"
-    if os.path.isdir(animal_dir):
-        entries = os.listdir(animal_dir)
-        dirs = [
-            entry for entry in entries if os.path.isdir(os.path.join(animal_dir, entry))
-        ]
-        dir_path = os.path.join(animal_dir, dirs[-1])
-        entries = os.listdir(dir_path)
-        last_csv = [
-            entry
-            for entry in entries
-            if os.path.isfile(os.path.join(dir_path, entry))
-            and re.match(r"^out_\d+\.csv$", entry)
-        ][-1]
+    while True:
+        update = input(
+            "Do you want to update animal.yml based on the last session? [y/n] "
+        )
+        update_lower = update.lower()
+        if update_lower == "y" or update_lower == "n":
+            break
 
-        df = pd.read_csv(os.path.join(dir_path, last_csv))
+    if update_lower == "y":
+        animal_dir = config["output_path"] + "/Rat" + f"{animal_config["animal_id"]:03}"
+        if os.path.isdir(animal_dir):
+            entries = os.listdir(animal_dir)
+            dirs = [
+                entry
+                for entry in entries
+                if os.path.isdir(os.path.join(animal_dir, entry))
+            ]
+            dir_path = os.path.join(animal_dir, dirs[-1])
+            entries = os.listdir(dir_path)
+            last_csv = [
+                entry
+                for entry in entries
+                if os.path.isfile(os.path.join(dir_path, entry))
+                and re.match(r"^out_\d+\.csv$", entry)
+            ][-1]
 
-        animal_config["session"]["number"] = int(
-            df.loc[df.index[-1], "session.number"] + 1
-        )
-        animal_config["session"]["starting_trial_number"] = int(
-            df.loc[df.index[-1], "trial.number"] + 1
-        )
-        animal_config["session"]["starting_block_number"] = int(
-            df.loc[df.index[-1], "block.number"] + 1
-        )
-        animal_config["fixation_time"]["opto_onset_time"]["min_value"] = float(
-            df.loc[df.index[-1], "fixation_time.opto_onset_time.base_time"]
-        )
-        animal_config["fixation_time"]["sound_onset_time"]["min_value"] = float(
-            df.loc[df.index[-1], "fixation_time.sound_onset_time.base_time"]
-        )
-        animal_config["reaction_time"]["min_value"] = float(
-            df.loc[df.index[-1], "reaction_time.base_time"]
-        )
-        animal_config["lnp_time"]["min_value"] = float(
-            df.loc[df.index[-1], "lnp_time.intended_duration"]
-        )
+            df = pd.read_csv(os.path.join(dir_path, last_csv))
 
-        yaml_string = (
-            "# yaml-language-server: $schema=schemas/animal-schema.json\n"
-            + yaml.dump(animal_config, default_flow_style=False)
-        )
-        with open(config["animal_path"], "w") as file:
-            file.write(yaml_string)
+            animal_config["session"]["number"] = int(
+                df.loc[df.index[-1], "session.number"] + 1
+            )
+            animal_config["session"]["starting_trial_number"] = int(
+                df.loc[df.index[-1], "trial.number"] + 1
+            )
+            animal_config["session"]["starting_block_number"] = int(
+                df.loc[df.index[-1], "block.number"] + 1
+            )
+            animal_config["fixation_time"]["opto_onset_time"]["min_value"] = float(
+                df.loc[df.index[-1], "fixation_time.opto_onset_time.base_time"]
+            )
+            animal_config["fixation_time"]["sound_onset_time"]["min_value"] = float(
+                df.loc[df.index[-1], "fixation_time.sound_onset_time.base_time"]
+            )
+            animal_config["reaction_time"]["min_value"] = float(
+                df.loc[df.index[-1], "reaction_time.base_time"]
+            )
+            animal_config["lnp_time"]["min_value"] = float(
+                df.loc[df.index[-1], "lnp_time.intended_duration"]
+            )
+
+            yaml_string = (
+                "# yaml-language-server: $schema=schemas/animal-schema.json\n"
+                + yaml.dump(animal_config, default_flow_style=False)
+            )
+            with open(config["animal_path"], "w") as file:
+                file.write(yaml_string)
 
     converter(config["setup_path"], "setup")
     converter(config["training_path"], "training")
