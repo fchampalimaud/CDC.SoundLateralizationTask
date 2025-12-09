3888f52cd778601efbd46a0535632bace9881c12
José Grilo <jose.grilo@research.fchampalimaud.org>
2025-12-09 14:38:08 +0000

    Refactor: launch the task's workflow in no-debug mode

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
 cd python
 uv run startup
 cd ..
-.\bonsai\Bonsai.exe .\src\SoundLateralizationTask.bonsai --start
+.\bonsai\Bonsai.exe .\src\SoundLateralizationTask.bonsai --start-no-debug
 cd python
 uv run convert-output
